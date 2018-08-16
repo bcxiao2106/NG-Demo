fdd45f3cab9ec51575575b1f71e4639bcc03382b
bichengxiao16@gmail.com <bichengxiao16@gmail.com>
2018-08-16 15:30:09 -0400

    Add header, sider nav bar

--- ng-demo/src/app/app.module.ts
+++ ng-demo/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SiderNavComponent } from './shared/components/sider-nav/sider-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SiderNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ng-demo/src/app/shared/components/sider-nav/sider-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sider-nav',
+  templateUrl: './sider-nav.component.html',
+  styleUrls: ['./sider-nav.component.css']
+})
+export class SiderNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
